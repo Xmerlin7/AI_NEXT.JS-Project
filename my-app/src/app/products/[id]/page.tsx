fda02390a09fb8441585336e968f123e19b0585c
Seif Allah <saifeldarageely@gmail.com>
2024-10-12 21:56:48 +0300

    Refactor page component names in docs and products pages

--- my-app/src/app/products/[id]/page.tsx
+++ my-app/src/app/products/[id]/page.tsx
@@ -1,4 +1,8 @@
+import {notFound} from "next/navigation"
 export default function ProductInfo({ params }: { params: { id: string } }) {
+  if (parseInt(params.id) > 1000){
+    notFound();
+  }
   return (
     <>
       <h1>Produc Info {params.id}</h1>
